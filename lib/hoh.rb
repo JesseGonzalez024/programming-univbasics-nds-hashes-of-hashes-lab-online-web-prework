dcf2a40ceb19789c411cf88ada665510f0919f80
Jesse Gonzalez <jessegonzalez024@gmail.com>
2019-10-29 02:45:12 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -44,11 +44,21 @@ def naming_system
 
   naming_system = {
     
-    label: "Kingdom" {label: "Phylum" {label "Class" {label: "Order" {label: "Family", {label: "Genus" {label: "Species", {sub_category: nil}
+    label: "Kingdom" 
+    {label: "Phylum" 
+    {label "Class" 
+    {label: "Order" 
+    {label: "Family" 
+    {label: "Genus" 
+    {label: "Species" 
+    {sub_category: nil}
       
-    }}}}}}
-    
-  }
+                }
+              }
+            }
+          }
+        }
+      }
   
 end
 
